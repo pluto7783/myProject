a3a33644de9e79d033131efbcccfbf5dca6a1ec0
pluto7783 <karolus@naver.com>
2019-05-23 15:12:03 +0900

    Add files via upload

---
+++ myProject/src/main/java/com/myproperty/mycode/main/MainController.java
@@ -0,0 +1,36 @@
+package com.myproperty.mycode.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@RequestMapping("main")
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "index.do", method = RequestMethod.GET)
+	public String index(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		model.addAttribute("myMsg", "This page is Main." );
+		
+		return "main/index";
+	}
+}
